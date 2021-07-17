387fa1557d52665295bfb0800745ed41a64b1071
brobles39 <85202635+brobles39@users.noreply.github.com>
2021-07-16 20:56:02 -0500

    terraform
    publicando archivos terraform

---
+++ terraform/vms.tf
@@ -0,0 +1,180 @@
+# Creamos la vm master
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "myVM1" {
+    name                = "master"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "pacman"
+    network_interface_ids = [ azurerm_network_interface.myNic1.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "pacman"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+
+# Creamos la vm worker1
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "myVM2" {
+    name                = "worker-1"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "pacman"
+    network_interface_ids = [ azurerm_network_interface.myNic2.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "pacman"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Creamos la vm worker2
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "myVM3" {
+    name                = "worker-2"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "pacman"
+    network_interface_ids = [ azurerm_network_interface.myNic3.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "pacman"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Creamos la vm NFS
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "myVM4" {
+    name                = "nfs"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "pacman"
+    network_interface_ids = [ azurerm_network_interface.myNic4.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username   = "pacman"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
