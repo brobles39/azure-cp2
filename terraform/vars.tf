7b99b7aa9c60ed5e1ede9ff764a50f41c53c135a
brobles39 <85202635+brobles39@users.noreply.github.com>
2021-07-17 09:18:21 -0500

    vm update

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,11 @@
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D2" # 8 GB, 2 CPU 
+  default = "Standard_D2_v2" # 8 GB, 2 CPU 
 }
 
 variable "vm_size2" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D2" # 8 GB, 2 CPU 
+  default = "Standard_D2_v2" # 8 GB, 2 CPU 
 }
