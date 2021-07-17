2c03db701b97bd7ce67847097bc1e1b7c6a33709
brobles39 <85202635+brobles39@users.noreply.github.com>
2021-07-17 00:32:49 -0500

    update security rules

--- terraform/sgs.tf
+++ terraform/sgs.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_interface_security_group_association" "mySecGroupAssoc
 
 }
 
-# Security group para worker-1
+# Security group para worker-1 servicio ssh
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
 
 resource "azurerm_network_security_group" "mySecGroup2" {
@@ -63,6 +63,40 @@ resource "azurerm_network_security_group" "mySecGroup2" {
 resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation2" {
     network_interface_id      = azurerm_network_interface.myNic2.id
     network_security_group_id = azurerm_network_security_group.mySecGroup2.id
+
+}
+
+# Security group para worker-1 puerto 80 http
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
+
+resource "azurerm_network_security_group" "mySecGroup21" {
+    name                = "httptraffic"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+}
+
+# Vinculamos el security group al interface de red
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_security_group_association
+
+resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation21" {
+    network_interface_id      = azurerm_network_interface.myNic2.id
+    network_security_group_id = azurerm_network_security_group.mySecGroup21.id
 
 }
 
@@ -97,6 +131,40 @@ resource "azurerm_network_security_group" "mySecGroup3" {
 resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation3" {
     network_interface_id      = azurerm_network_interface.myNic3.id
     network_security_group_id = azurerm_network_security_group.mySecGroup3.id
+
+}
+
+# Security group para worker-2 puerto 80 http
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
+
+resource "azurerm_network_security_group" "mySecGroup31" {
+    name                = "httptraffic2"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+}
+
+# Vinculamos el security group al interface de red
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_security_group_association
+
+resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation31" {
+    network_interface_id      = azurerm_network_interface.myNic3.id
+    network_security_group_id = azurerm_network_security_group.mySecGroup31.id
 
 }
 
